21f15d22d89773e771d7124ab74f6e400829d460
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-08-07 18:36:00 +0200

    move files

---
+++ NFT/test/unit/DeployMoodNftTest.t.sol
@@ -0,0 +1,15 @@
+// // SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MoodNft} from "../../src/MoodNft.sol";
+import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft public deployer;
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+    }
+}
