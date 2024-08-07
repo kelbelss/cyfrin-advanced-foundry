b33711867a063f5c0b9007d317c6e1f75377f9b7
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-08-07 18:36:21 +0200

    Update DeployMoodNft.s.sol

--- NFT/script/DeployMoodNft.s.sol
+++ NFT/script/DeployMoodNft.s.sol
@@ -5,9 +5,20 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 import {MoodNft} from "../src/MoodNft.sol";
+import {console} from "forge-std/console.sol";
 
 contract DeployMoodNft is Script {
-    function run() external returns (MoodNft) {}
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./img/sad.svg");
+        string memory happySvg = vm.readFile("./img/happy.svg");
+
+        console.log(sadSvg);
+
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(svgToImageURI(sadSvg), svgToImageURI(happySvg));
+        vm.stopBroadcast();
+        return moodNft;
+    }
 
     function svgToImageURI(string memory svg) public pure returns (string memory) {
         string memory baseURI = "data:image/svg+xml;base64,";
