9f6674624f8bf1307366ecf49d6506780cca6a66
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-08-13 15:08:53 +0200

    Complete initial contract

--- Stablecoin/src/DecentralisedStablecoin.sol
+++ Stablecoin/src/DecentralisedStablecoin.sol
@@ -1,4 +1,69 @@
 // SPDX-License-Identifier: UNLICENSED
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
 pragma solidity ^0.8.18;
 
-contract DecentralisedStablecoin {}
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/*
+ * @title DecentralizedStableCoin
+ * @author Kelbels
+ * Collateral: Exogenous
+ * Stability Mechanism: Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto (wETH and wBTC)
+ *
+* This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the
+DSCEngine smart contract.
+ */
+contract DecentralisedStableCoin is ERC20Burnable, Ownable {
+    error DecentralisedStableCoin__MustBeMoreThanZero();
+    error DecentralisedStableCoin__BurnAmountExceedsBalance();
+    error DecentralisedStableCoin__NotZeroAddress();
+
+    constructor() ERC20("DecentralisedStableCoin", "DSC") Ownable(0x1AB1d79060b917FaaD7EE152EbCebE73b6F347b1) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralisedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralisedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount); // use burn function from parents class (ERC20Burnable)
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralisedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralisedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
