c8e015e337eae2e2555a78ae76cf09d8fc617c5c
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-08-27 17:46:25 +0200

    update invariants test

--- Stablecoin/test/fuzz/failOnRevert/InvariantsTest.t.sol
+++ Stablecoin/test/fuzz/failOnRevert/InvariantsTest.t.sol
@@ -3,14 +3,12 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
-import {DSCEngine} from "../../src/DSCEngine.sol";
-import {DecentralisedStableCoin} from "../../src/DecentralisedStableCoin.sol";
-import {HelperConfig} from "../../script/HelperConfig.s.sol";
-import {DeployDSC} from "../../script/DeployDSC.s.sol";
-// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+import {DSCEngine} from "../../../src/DSCEngine.sol";
+import {DecentralisedStableCoin} from "../../../src/DecentralisedStableCoin.sol";
+import {HelperConfig} from "../../../script/HelperConfig.s.sol";
+import {DeployDSC} from "../../../script/DeployDSC.s.sol";
+import {ERC20Mock} from "../../mocks/ERC20Mock.sol";
 // import { StopOnRevertHandler } from "./StopOnRevertHandler.t.sol";
-// import { console } from "forge-std/console.sol";
 
 contract InvariantsTest is StdInvariant, Test {
     DeployDSC public deployer;
