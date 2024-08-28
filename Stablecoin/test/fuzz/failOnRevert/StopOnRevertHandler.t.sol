ab83e7364ca550ecdde64bbb74422ea47983c288
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-08-28 13:00:09 +0200

    set up handler and add functions to interact with

---
+++ Stablecoin/test/fuzz/failOnRevert/StopOnRevertHandler.t.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+import {ERC20Mock} from "../../mocks/ERC20Mock.sol";
+
+import {MockV3Aggregator} from "../../mocks/MockV3Aggregator.sol";
+import {DSCEngine, AggregatorV3Interface} from "../../../src/DSCEngine.sol";
+import {DecentralisedStableCoin} from "../../../src/DecentralisedStableCoin.sol";
+import {MockV3Aggregator} from "../../mocks/MockV3Aggregator.sol";
+
+contract StopOnRevertHandler is Test {
+    // Deployed contracts to interact with
+    DSCEngine public dscEngine;
+    DecentralisedStableCoin public dsc;
+    MockV3Aggregator public ethUsdPriceFeed;
+    MockV3Aggregator public btcUsdPriceFeed;
+    ERC20Mock public weth;
+    ERC20Mock public wbtc;
+
+    uint96 public constant MAX_DEPOSIT_SIZE = type(uint96).max;
+
+    constructor(DSCEngine _dscEngine, DecentralisedStableCoin _dsc) {
+        dscEngine = _dscEngine;
+        dsc = _dsc;
+
+        address[] memory collateralTokens = dscEngine.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
+
+        ethUsdPriceFeed = MockV3Aggregator(dscEngine.getCollateralTokenPriceFeed(address(weth)));
+        btcUsdPriceFeed = MockV3Aggregator(dscEngine.getCollateralTokenPriceFeed(address(wbtc)));
+    }
+
+    ///////////////
+    // DSCEngine //
+    ///////////////
+    function mintAndDepositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        // must be more than 0
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(dscEngine), amountCollateral);
+        dscEngine.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
+    }
+
+    function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        uint256 maxCollateral = dscEngine.getCollateralBalanceOfUser(msg.sender, address(collateral));
+
+        amountCollateral = bound(amountCollateral, 0, maxCollateral);
+
+        if (amountCollateral == 0) {
+            return;
+        }
+        vm.prank(msg.sender);
+        dscEngine.redeemCollateral(address(collateral), amountCollateral);
+    }
+
+    ////////////////////////
+    /// Helper Functions ///
+    ////////////////////////
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+        if (collateralSeed % 2 == 0) {
+            return weth;
+        } else {
+            return wbtc;
+        }
+    }
+}
