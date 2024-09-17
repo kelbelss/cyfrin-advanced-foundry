7f623ee76cfb4f1b2a72dc267d0921100010386f
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-09-17 17:30:24 +0200

    add constructor and begin claim function

--- Airdrop/src/MerkleAirdrop.sol
+++ Airdrop/src/MerkleAirdrop.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.24;
 
 // import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-// import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-// import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 // import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 // import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 // import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
@@ -13,5 +13,18 @@ pragma solidity ^0.8.24;
  * @author Cyfrin
  */
 contract MerkleAirdrop is EIP712 {
-// merkle proofs - merkle tree (data structure) prove that some piece of data is in a group of data we have without iterating through a massive array
+    // merkle proofs - merkle tree (data structure) prove that some piece of data is in a group of data we have without iterating through a massive array
+
+    IERC20 private immutable i_airdropToken;
+    bytes32 private immutable i_merkleRoot;
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) EIP712("Merkle Airdrop", "1.0.0") {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // calculate using the account and the amount, the hash -> leaf node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+    }
 }
