6c39b09ca59b625b188893fdfe2ed772e402ad29
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-09-17 16:53:51 +0200

    Create MerkleAirdrop.sol

---
+++ Airdrop/src/MerkleAirdrop.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+// import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+// import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+// import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+// import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+// import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+/**
+ * @title Merkle Airdrop - Airdrop tokens to users who can prove they are in a merkle tree
+ * @author Cyfrin
+ */
+contract MerkleAirdrop is EIP712 {
+// merkle proofs - merkle tree (data structure) prove that some piece of data is in a group of data we have without iterating through a massive array
+}
