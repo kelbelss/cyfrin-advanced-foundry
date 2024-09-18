2a7c2de93a208ff308f648195ed36b092c017f13
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-09-18 13:34:56 +0200

    airdrop tests

---
+++ Airdrop/test/MerkleAirdrop.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+contract MerkleAirdropTest is Test {
+    MerkleAirdrop airdrop;
+    BagelToken token;
+
+    address user;
+    uint256 userPrivKey;
+
+    bytes32 merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 amountToCollect = (25 * 1e18); // 25.000000
+    uint256 amountToSend = amountToCollect * 4;
+
+    bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] proof = [proofOne, proofTwo];
+
+    function setUp() public {
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(merkleRoot, token);
+        // create supply
+        token.mint(token.owner(), amountToSend);
+        token.transfer(address(airdrop), amountToSend);
+        (user, userPrivKey) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(user);
+
+        vm.prank(user);
+        airdrop.claim(user, amountToCollect, proof);
+
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("Ending balance", endingBalance);
+        assertEq(endingBalance - startingBalance, amountToCollect);
+    }
+}
