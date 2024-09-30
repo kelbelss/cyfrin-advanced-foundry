26e3c185f79a64a0f2e73055310a54dc3333c3dc
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-09-30 11:02:32 +0200

    upgradable smart contracts

---
+++ Upgrades/src/BoxV1.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    uint256 internal value;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(
+            /**
+             * address *
+             */
+        );
+        __UUPSUpgradeable_init();
+    }
+
+    function getValue() public view returns (uint256) {
+        return value;
+    }
+
+    function version() public pure returns (uint256) {
+        return 1;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
---
+++ Upgrades/src/BoxV2.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    uint256 internal value;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(
+            /**
+             * address *
+             */
+        );
+        __UUPSUpgradeable_init();
+    }
+
+    function setValue(uint256 newValue) public {
+        value = newValue;
+    }
+
+    function getValue() public view returns (uint256) {
+        return value;
+    }
+
+    function version() public pure returns (uint256) {
+        return 2;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
