694250d81e2734340d8caa18b3eb0ac8f7d01c68
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-09-30 11:22:37 +0200

    update initialize

--- Upgrades/src/BoxV1.sol
+++ Upgrades/src/BoxV1.sol
@@ -8,16 +8,13 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/U
 contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal value;
 
+    // @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
     function initialize() public initializer {
-        __Ownable_init(
-            /**
-             * address *
-             */
-        );
+        __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
     }
 
