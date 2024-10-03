886beef69e2eb4f35a70f71bc25fe81fda41fe87
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-10-03 11:04:15 +0200

    set up minimal account contract

---
+++ AccountAbstraction/src/MinimalAccount.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import {IAccount} from "../lib/account-abstraction/contracts/interfaces/IAccount.sol";
+
+contract MinimalAccount is IAccount {
+    // entry point contract will call this one
+
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {}
+}
