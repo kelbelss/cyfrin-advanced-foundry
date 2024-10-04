06ba21019f07c6a1116e4ea1c6cb81187007248b
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-10-04 12:39:30 +0200

    import and add ownable

--- AccountAbstraction/src/MinimalAccount.sol
+++ AccountAbstraction/src/MinimalAccount.sol
@@ -3,9 +3,12 @@ pragma solidity 0.8.24;
 
 import {IAccount} from "../lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "../lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MinimalAccount is IAccount {
+contract MinimalAccount is IAccount, Ownable {
     // entry point contract will call this one
+
+    constructor() Ownable(msg.sender) {}
 
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
