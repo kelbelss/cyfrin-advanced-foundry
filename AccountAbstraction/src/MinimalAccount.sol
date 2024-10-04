dd6484b9218e29c80e31cc2a95b20a901dec8e23
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-10-04 12:26:44 +0200

    import userPackedOperation

--- AccountAbstraction/src/MinimalAccount.sol
+++ AccountAbstraction/src/MinimalAccount.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.24;
 
 import {IAccount} from "../lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "../lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 
 contract MinimalAccount is IAccount {
     // entry point contract will call this one
