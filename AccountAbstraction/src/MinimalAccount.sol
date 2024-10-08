a8a804b65373e095b8e56e8f108dc2f96ff21db3
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-10-08 11:28:32 +0200

    validate UserOp

--- AccountAbstraction/src/MinimalAccount.sol
+++ AccountAbstraction/src/MinimalAccount.sol
@@ -4,6 +4,9 @@ pragma solidity 0.8.24;
 import {IAccount} from "../lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "../lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "../lib/account-abstraction/contracts/core/Helpers.sol";
 
 contract MinimalAccount is IAccount, Ownable {
     // entry point contract will call this one
@@ -14,7 +17,9 @@ contract MinimalAccount is IAccount, Ownable {
         external
         returns (uint256 validationData)
     {
-        _validateSignature(userOp, userOpHash);
+        validationData = _validateSignature(userOp, userOpHash);
+        // should _validateNonce()
+        _payPrefund(missingAccountFunds);
     }
 
     // EIP-191 version of the signed hash
@@ -22,5 +27,19 @@ contract MinimalAccount is IAccount, Ownable {
         internal
         view
         returns (uint256 validationData)
-    {}
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds != 0) {
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
+            (success);
+        }
+    }
 }
