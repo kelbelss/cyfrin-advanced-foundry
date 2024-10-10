3c2fd6c9b587b0af7a10dad7ada8c1da62f95d28
kelbelss <124790103+kelbelss@users.noreply.github.com>
2024-10-10 09:43:57 +0200

    set up helper config

---
+++ AccountAbstraction/script/HelperConfig.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {EntryPoint} from "../lib/account-abstraction/contracts/core/EntryPoint.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract HelperConfig is Script {}
